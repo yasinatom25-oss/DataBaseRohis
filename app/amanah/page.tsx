d00411e67d5f9f786a112c104c2dd6cdab5473c9
Agys <agys@rohis.id>
2026-06-05 09:20:04 +0700

    fix: conditionally show Add Amanah button in AmanahList for authorized users only

--- app/amanah/page.tsx
+++ app/amanah/page.tsx
@@ -182,7 +182,7 @@ export default function AmanahPage() {
                     {group.tasks.length}
                   </span>
                 </h3>
-                <AmanahList tasks={group.tasks} onTaskClick={(task) => setSelectedTask(task)} onAddAmanah={() => setIsCreateModalOpen(true)} />
+                <AmanahList tasks={group.tasks} onTaskClick={(task) => setSelectedTask(task)} onAddAmanah={canCreateRecords(user.role.name) ? () => setIsCreateModalOpen(true) : undefined} />
               </div>
             ))
           )}
